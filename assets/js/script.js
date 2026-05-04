ba4c2a0b57ee58575056a18d421ed3ea9088555d
Natnael20 <natexwedisuplex67@gmail.com>
2026-05-04 16:11:04 +0200

    created wishlist and set up the header for it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const searchIcon = document.getElementById('search-icon');
-    const searchBox = document.querySelector('.search-box');
-
-    let boxOn = false;
-    // Hide the search box by default
-    searchBox.style.display = "none";
-
-    searchIcon.addEventListener('click', () => {
-        if(boxOn) {
-            searchBox.style.display = "none";
-            boxOn = false;
-        } else {
-            searchBox.style.display = "block";
-            boxOn = true;
-        }
-    });
-
-    document.addEventListener('keydown', function(event) {
-        if (event.key === "Escape") {
-            searchBox.style.display = "none";
-        }
-    });
-
 
     const addToCartButtons = document.querySelectorAll('.img-shop a');
     const shoppingCartSpan = document.querySelector('.shopping-cart a span');
---
+++ assets/js/search.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchIcon = document.getElementById('search-icon');
+    const searchBox = document.querySelector('.search-box');
+
+    let boxOn = false;
+    // Hide the search box by default
+    searchBox.style.display = "none";
+
+    searchIcon.addEventListener('click', () => {
+        if(boxOn) {
+            searchBox.style.display = "none";
+            boxOn = false;
+        } else {
+            searchBox.style.display = "block";
+            boxOn = true;
+        }
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === "Escape") {
+            searchBox.style.display = "none";
+        }
+    });
+});
